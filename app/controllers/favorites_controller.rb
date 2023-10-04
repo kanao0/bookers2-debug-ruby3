2b055de7dc6758785763b9ca737b100ecadc67af
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-10-04 09:20:13 +0000

    [finish]いいね非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,24 +1,21 @@
 class FavoritesController < ApplicationController
-  
+
   def create
     # これからいいねをする本の情報
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     # 今のユーザーからいいねの情報を取得して新しいいいねを作成する（favoriteモデルの中のbook_idに対して：これからいいねをする本の情報　を代入）★どの☆
-    favorite = current_user.favorites.new(book_id: book.id)
+    favorite = current_user.favorites.new(book_id: @book.id)
     # いいねを保存
     favorite.save
     # ページを切り替え
-    redirect_to book_path(book.id)
   end
- 
- 
+
+
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to book_path(book.id)
   end
- 
+
 end
 
- 
